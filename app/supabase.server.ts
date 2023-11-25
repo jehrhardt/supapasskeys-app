62784644d7817c44d6f097e90e44a268cb17b8d4
Jan Ehrhardt <59441+jehrhardt@users.noreply.github.com>
2023-11-25 21:15:41 +0100

    Fix formatting

--- app/supabase.server.ts
+++ app/supabase.server.ts
@@ -1,11 +1,18 @@
-import { createServerClient, parse, serialize } from "https://esm.sh/@supabase/ssr@0.0.10";
+import {
+  createServerClient,
+  parse,
+  serialize,
+} from "https://esm.sh/@supabase/ssr@0.0.10";
 import { SupabaseClient } from "https://esm.sh/@supabase/supabase-js@2.38.5";
 import { load } from "https://deno.land/std@0.207.0/dotenv/mod.ts";
 import { ActionFunctionArgs } from "@remix-run/deno";
 
-// deno-lint-ignore no-explicit-any
-export default async function supabaseClient(request: ActionFunctionArgs, headers: Headers): Promise<SupabaseClient<any, "public", any>> {
-  const cookies = parse(request.headers.get('Cookie') ?? '')
+export default async function supabaseClient(
+  request: ActionFunctionArgs,
+  headers: Headers = new Headers(),
+  // deno-lint-ignore no-explicit-any
+): Promise<SupabaseClient<any, "public", any>> {
+  const cookies = parse(request.headers.get("Cookie") ?? "");
 
   const env = await load();
   const supabaseUrl = env["SUPABASE_URL"];
@@ -14,14 +21,14 @@ export default async function supabaseClient(request: ActionFunctionArgs, header
   return createServerClient(supabaseUrl, supabaseAnonKey, {
     cookies: {
       get(key) {
-        return cookies[key]
+        return cookies[key];
       },
       set(key, value, options) {
-        headers.append('Set-Cookie', serialize(key, value, options))
+        headers.append("Set-Cookie", serialize(key, value, options));
       },
       remove(key, options) {
-        headers.append('Set-Cookie', serialize(key, '', options))
+        headers.append("Set-Cookie", serialize(key, "", options));
       },
     },
-  })
+  });
 }
